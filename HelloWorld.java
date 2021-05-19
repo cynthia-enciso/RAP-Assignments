f35c628aea9e25c62a242c9f8522db24ab957e2a
Cynthia Enciso <cynthia.enciso@revature.com>
2021-05-19 10:51:42 -0500

    First program

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    // this is a single-line comment.
+    // this is the main method
+    public static void main(String[] args) {
+        /*
+        This is a multi-line comment.
+         */
+        System.out.println("Hello World");
+    }
+}
